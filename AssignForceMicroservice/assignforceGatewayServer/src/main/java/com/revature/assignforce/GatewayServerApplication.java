6a7919a27db078be9ef71c411a3ab716ad45929c
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2018-07-12 15:38:37 -0400

    Zachary chandler altered properties files

--- AssignForceMicroservice/assignforceGatewayServer/src/main/java/com/revature/assignforce/GatewayServerApplication.java
+++ AssignForceMicroservice/assignforceGatewayServer/src/main/java/com/revature/assignforce/GatewayServerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableZuulProxy
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="http://35.173.212.152:4200")
 public class GatewayServerApplication {
 	
 	public static void main(String[] args) throws Exception {
